5d33574752d8c667b85528166194a686ddfd4b73
Bimal10985 <bimal10985@gmail.com>
2023-07-02 08:38:50 +0545

    connected to db

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Backend is working");
 });
 
-app.listen(8000, () => {
+app.listen(8000, async () => {
   console.log("Server started");
+  try {
+    await mongoose.connect(
+      "mongodb+srv://bimal10985:CcRTddu1h1NI4DSa@cluster0.dcrhemp.mongodb.net/expensestracker?retryWrites=true&w=majority"
+    );
+    console.log("Connected to database");
+  } catch (error) {
+    console.log(error);
+  }
 });
